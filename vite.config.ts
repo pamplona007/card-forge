8c2b3f7b42dc6a0d16e4a6cc1200b48045462826
Lucas Pamplona <pamplona.developer@gmail.com>
2026-03-11 20:55:59 -0300

    Fixed vercel routing

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-    appType: 'spa',
     plugins: [
         react(),
         tsconfigPaths(),
